cdc590beb5a08ddfa5aaf61a64ca1ca54cb44488
Gustavo Pandolpho <gwprkg@gmail.com>
2023-12-20 13:36:40 -0300

    feat: realização dos desafios

--- src/main/java/br/com/dio/desafioreactor/Question2.java
+++ src/main/java/br/com/dio/desafioreactor/Question2.java
@@ -9,6 +9,14 @@ public class Question2 {
     /*
     Recebe uma lista de usuários e retorna a quantos usuários admin tem na lista
      */
-    public Mono<Long> countAdmins(final List<User> users){}
+    public Mono<Long> countAdmins(final List<User> users){
+        Long countUserAdmin = 0L;
+        for(User user : users){
+            if(user.isAdmin()){
+                countUserAdmin += 1L;
+            }
+        }
+        return Mono.just(countUserAdmin);
+    }
 
 }
